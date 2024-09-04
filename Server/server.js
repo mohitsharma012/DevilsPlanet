e40a1b98318cd091458fb40aeb23975d9144f598
mohitsharma012 <stayer.mohit@gmail.com>
2024-09-04 15:23:33 +0530

    sdf

--- Server/server.js
+++ Server/server.js
@@ -1,49 +1,44 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 
 // Database connection
-import connectDB from './Config/db.js';
+import connectDB from "./Config/db.js";
 
 // Routes Import
-import isAuthorized from './Middleware/isAuthorized.js';
-import ProductRouter from './Routes/ProductRoute.js';
-import userRoute from './Routes/userRoute.js';
-import cartRoute from './Routes/cartRoute.js';
+import isAuthorized from "./Middleware/isAuthorized.js";
+import ProductRouter from "./Routes/ProductRoute.js";
+import userRoute from "./Routes/userRoute.js";
+import cartRoute from "./Routes/cartRoute.js";
 
-const app = express();                  
+const app = express();
 dotenv.config();
 
 const corsOptions = {
-  origin: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
+  origin: 'https://devils-planet-bu1aiydse-mohits-projects-ed99787c.vercel.app',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // Allow cookies to be sent with requests
 };
-app.use(cors(
-corsOptions
-));
+
+app.use(cors(corsOptions));
+
+
 // middlewares
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // database connection
 connectDB();
 
 // api endpoints
-app.use('/api/product', ProductRouter);
-app.use('/api/user', userRoute);
-app.use('/api/cart', isAuthorized ,cartRoute);
+app.use("/api/product", ProductRouter);
+app.use("/api/user", userRoute);
+app.use("/api/cart", isAuthorized, cartRoute);
 
-
-
-
-
-
-
-app.get('/', (req, res) => {
-  res.send('Server is working');
+app.get("/", (req, res) => {
+  res.send("Server is working");
 });
 
 app.listen(3000, () => {
-  console.log('Server is running on', process.env.URL + process.env.PORT);
+  console.log("Server is running on", process.env.URL + process.env.PORT);
 });
